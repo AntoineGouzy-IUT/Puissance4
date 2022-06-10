5e30192965688a03f7a9351f29740b6d5a4d5888
Courbaize Romain <romain.courbaize@iut-rodez.fr>
2022-06-10 10:25:23 +0200

    Modification return ordinateur

--- src/application/Partie.java
+++ src/application/Partie.java
@@ -52,17 +52,19 @@ public class Partie implements Serializable {
 	 * Défend  - si le joueur humain a 3 pions alignés
 	 * Attaque - si l'IA a 3 pions alignés, elle gagne
 	 *           si non, elle complète la ligne, la colonne ou la diagonale
+	 * @return sous la forme d'un tableau 
+	 *         {colonne, ligne, le résultat de la vérification}
 	 */
-	public void ordinateur() {
+	public int[] ordinateur() {
 		
 		int resultatDefense;
 		
 		resultatDefense = defense();
 		
 		if (resultatDefense > -1) {
-			ajouterPion(2, resultatDefense);
+			return ajouterPion(2, resultatDefense);
 		} else {
-			ajouterPion(2, attaque());
+			return ajouterPion(2, attaque());
 		}
 	}
 	
@@ -230,8 +232,6 @@ public class Partie implements Serializable {
 			
 		}
 		grille[colonne][noVide] = joueur;
-		
-		verifierAlignement(colonne, noVide);
 		
 		int[] resultat = {colonne, noVide, verifierAlignement(colonne, noVide)};
 		return resultat;
